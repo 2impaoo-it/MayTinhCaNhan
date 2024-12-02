6be4fc14159ba254c3f3279d3df38fe06c0721ed
2impaoo <baoga271104@gmail.com>
2024-12-02 12:43:58 +0700

    Add project files.

---
+++ FrmMain.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace MayTinhCaNhan
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtFirstNum = new System.Windows.Forms.TextBox();
+            this.txtSecondNum = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnCong = new System.Windows.Forms.Button();
+            this.btnTru = new System.Windows.Forms.Button();
+            this.btnNhan = new System.Windows.Forms.Button();
+            this.btnChia = new System.Windows.Forms.Button();
+            this.errNhapSo = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errNhapSo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Font = new System.Drawing.Font("Mistral", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(205, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(174, 56);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Caculator";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(12, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "First number:";
+            // 
+            // txtFirstNum
+            // 
+            this.txtFirstNum.Location = new System.Drawing.Point(184, 112);
+            this.txtFirstNum.Multiline = true;
+            this.txtFirstNum.Name = "txtFirstNum";
+            this.txtFirstNum.Size = new System.Drawing.Size(390, 42);
+            this.txtFirstNum.TabIndex = 2;
+            this.txtFirstNum.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtFirstNum_KeyPress);
+            this.txtFirstNum.Validating += new System.ComponentModel.CancelEventHandler(this.txtFirstNum_Validating);
+            // 
+            // txtSecondNum
+            // 
+            this.txtSecondNum.Location = new System.Drawing.Point(184, 177);
+            this.txtSecondNum.Multiline = true;
+            this.txtSecondNum.Name = "txtSecondNum";
+            this.txtSecondNum.Size = new System.Drawing.Size(390, 42);
+            this.txtSecondNum.TabIndex = 2;
+            this.txtSecondNum.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtSecondNum_KeyPress);
+            this.txtSecondNum.Validating += new System.ComponentModel.CancelEventHandler(this.txtSecondNum_Validating);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(12, 194);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(157, 25);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Second number:";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(184, 257);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(390, 42);
+            this.txtResult.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(12, 274);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Result:";
+            // 
+            // btnCong
+            // 
+            this.btnCong.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCong.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCong.Location = new System.Drawing.Point(85, 341);
+            this.btnCong.Name = "btnCong";
+            this.btnCong.Size = new System.Drawing.Size(75, 66);
+            this.btnCong.TabIndex = 4;
+            this.btnCong.Text = "+";
+            this.btnCong.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnCong.UseVisualStyleBackColor = true;
+            this.btnCong.Click += new System.EventHandler(this.btnCaculator_Click);
+            // 
+            // btnTru
+            // 
+            this.btnTru.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTru.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTru.Location = new System.Drawing.Point(196, 341);
+            this.btnTru.Name = "btnTru";
+            this.btnTru.Size = new System.Drawing.Size(75, 66);
+            this.btnTru.TabIndex = 4;
+            this.btnTru.Text = "-";
+            this.btnTru.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnTru.UseVisualStyleBackColor = true;
+            this.btnTru.Click += new System.EventHandler(this.btnCaculator_Click);
+            // 
+            // btnNhan
+            // 
+            this.btnNhan.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNhan.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNhan.Location = new System.Drawing.Point(304, 341);
+            this.btnNhan.Name = "btnNhan";
+            this.btnNhan.Size = new System.Drawing.Size(75, 66);
+            this.btnNhan.TabIndex = 4;
+            this.btnNhan.Text = "x";
+            this.btnNhan.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnNhan.UseVisualStyleBackColor = true;
+            this.btnNhan.Click += new System.EventHandler(this.btnCaculator_Click);
+            // 
+            // btnChia
+            // 
+            this.btnChia.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnChia.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChia.Location = new System.Drawing.Point(416, 341);
+            this.btnChia.Name = "btnChia";
+            this.btnChia.Size = new System.Drawing.Size(75, 66);
+            this.btnChia.TabIndex = 4;
+            this.btnChia.Text = "/";
+            this.btnChia.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnChia.UseVisualStyleBackColor = true;
+            this.btnChia.Click += new System.EventHandler(this.btnCaculator_Click);
+            // 
+            // errNhapSo
+            // 
+            this.errNhapSo.ContainerControl = this;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(602, 450);
+            this.Controls.Add(this.btnChia);
+            this.Controls.Add(this.btnNhan);
+            this.Controls.Add(this.btnTru);
+            this.Controls.Add(this.btnCong);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtSecondNum);
+            this.Controls.Add(this.txtFirstNum);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.IndianRed;
+            this.Name = "FrmMain";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.errNhapSo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtFirstNum;
+        private System.Windows.Forms.TextBox txtSecondNum;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnCong;
+        private System.Windows.Forms.Button btnTru;
+        private System.Windows.Forms.Button btnNhan;
+        private System.Windows.Forms.Button btnChia;
+        private System.Windows.Forms.ErrorProvider errNhapSo;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
+
---
+++ FrmMain.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MayTinhCaNhan
+{
+    public partial class FrmMain : Form
+    {   
+        public FrmMain()
+        {
+            //Console.OutputEncoding = Encoding.UTF8;
+            InitializeComponent();
+        }
+
+        private void btnCaculator_Click(object sender, EventArgs e)
+        {
+            if(errNhapSo.GetError(txtFirstNum) != string.Empty || (errNhapSo.GetError(txtSecondNum) != string.Empty))
+            {
+                MessageBox.Show("Vui lòng kiểm tra lại thông tin đã nhập", "Thông báo");
+                return;
+            }
+
+            double a = double.Parse(txtFirstNum.Text);
+            double b = double.Parse(txtSecondNum.Text);
+
+            Button btn = (Button)sender;
+            double results = 0;
+            switch (btn.Name)
+            {
+                case "btnCong":
+                    results = a + b;
+                    break;
+                case "btnTru":
+                    results = a - b;
+                    break;
+                case "btnNhan":
+                    results = a * b;
+                    break;
+                case "btnChia":
+                    if (b == 0)
+                    {
+                        MessageBox.Show("Phân số không hợp lệ");
+                        return;
+                    }
+                    results = a / b;
+                    break;
+            }
+            txtResult.Text = results.ToString();
+        }
+
+        private void txtFirstNum_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Kiểm tra xem phím vừa nhập vào có phải là số không, được phép nhập số âm, số thập phân
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar) && e.KeyChar != '-' && e.KeyChar != ',')
+            {
+                e.Handled = true;
+                errNhapSo.SetError(txtFirstNum, "Chỉ cho phép nhập số!");
+            }
+            else
+                errNhapSo.SetError(txtFirstNum, string.Empty);
+
+        }
+
+        private void txtSecondNum_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar) && e.KeyChar != '-' && e.KeyChar != ',')
+            {
+                e.Handled = true;
+                errNhapSo.SetError(txtSecondNum, "Chỉ cho phép nhập số!");
+            }
+            else
+                errNhapSo.SetError(txtSecondNum, string.Empty);
+        }
+
+        private void txtFirstNum_Validating(object sender, CancelEventArgs e)
+        {
+                if (string.IsNullOrEmpty(txtFirstNum.Text))
+                {
+                    errNhapSo.SetError(txtFirstNum, "Không được để trống!");
+                }
+                else
+                    errNhapSo.SetError(txtFirstNum, string.Empty);
+            
+        }
+
+        private void txtSecondNum_Validating(object sender, CancelEventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtSecondNum.Text))
+            {
+                errNhapSo.SetError(txtSecondNum, "Không được để trống!");
+            }
+            else
+                errNhapSo.SetError(txtSecondNum, string.Empty);
+        }
+    }
+}
